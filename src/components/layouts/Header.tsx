c61f5dd56600f8d7fbf0da42d8cfec3570cc3a1e
nakmiers <diwas118151@gmail.com>
2024-09-15 00:22:27 +0700

    remove session-load-page

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -112,19 +112,19 @@ function Header({ className = '' }: HeaderProps) {
     return () => window.removeEventListener('scroll', handleScroll)
   }, [pathname])
 
-  // update user session after load page and every 60s
-  useEffect(() => {
-    // update session after load page
-    const updateSession = async () => {
-      console.log('Session - load-page...')
-      await update()
-      isUpdatedSession.current = true
-    }
+  // // update user session after load page and every 60s
+  // useEffect(() => {
+  //   // update session after load page
+  //   const updateSession = async () => {
+  //     console.log('Session - load-page...')
+  //     await update()
+  //     isUpdatedSession.current = true
+  //   }
 
-    if (!isUpdatedSession.current) {
-      updateSession()
-    }
-  }, [update, session])
+  //   if (!isUpdatedSession.current) {
+  //     updateSession()
+  //   }
+  // }, [update, session])
 
   return (
     <header
