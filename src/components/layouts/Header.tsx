0d72b142abe59aff89482e8ac3b6503da634646c
nakmiers <diwas118151@gmail.com>
2024-10-07 13:13:17 +0700

    change ads method

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -50,7 +50,7 @@ function Header({ className = '' }: HeaderProps) {
 
   // MARK: ADS
   useEffect(() => {
-    const showTime = 10000
+    const showTime = 8000
     const interval = 30000
 
     setTimeout(() => {
@@ -149,11 +149,8 @@ function Header({ className = '' }: HeaderProps) {
       } trans-300 bottom-0 w-full bg-dark-100 text-light md:bottom-auto md:top-0 ${className}`}
     >
       {/* Ads */}
-      <Link
-        href="https://monaedu.com"
-        target="_blank"
-        rel="noreferrer"
-        className={`${openAds ? 'max-h-[200px] py-0.5 sm:max-h-12 md:max-h-6' : 'max-h-0 py-0'} trans-300 group block w-full overflow-hidden bg-gradient-to-t from-[#2f2e3e] to-primary px-3 text-center font-body text-sm tracking-wider text-light`}
+      <div
+        className={`${openAds ? 'max-h-[200px] py-0.5 sm:max-h-12 md:max-h-6' : 'max-h-0 py-0'} trans-300 group block w-full overflow-hidden bg-gradient-to-t ${isTransparent ? 'from-transparent' : 'from-[#2f2e3e]'} to-primary px-3 text-center font-body text-sm tracking-wider text-light`}
         title='Giảm đến 100.000đ hoặc 50% khi nhập mã "BIGSALE50" học tại monaedu.com'
       >
         <p className="">
@@ -163,7 +160,7 @@ function Header({ className = '' }: HeaderProps) {
           </span>{' '}
           giảm ngay <span className="font-semibold">50%</span>
         </p>
-      </Link>
+      </div>
 
       {/* Main Header */}
       <div className="trans-300 relative m-auto flex h-[72px] w-full max-w-1200 items-center justify-between gap-1 px-3 sm:px-21">
